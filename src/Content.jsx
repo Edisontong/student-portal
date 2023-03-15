a3b6bde4e900a77ae11975b5a12610d53583e4eb
Nicholas Gonzalez <thisnickg194@gmail.com>
2023-03-15 14:21:32 -0500

    commit

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { Login } from "./Login";
+
+import { Routes, Route } from "react-router-dom";
 
 export function Content() {
   const logins = [{ id: 1, email: "test@test.com", password: "password" }];
   return (
     <div>
-      <Login logins={logins} />
       <h1>Welcome to our resume app!</h1>
+      <Routes>
+        <Route path="/login" element={<Login logins={logins} />} />
+      </Routes>
     </div>
   );
 }
