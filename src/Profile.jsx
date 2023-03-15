eb21d26b4f90b9c68a34394a021131e86ce737fa
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-03-15 14:10:14 -0600

    adding bootstrap containers

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -7,17 +7,23 @@ export function Profile() {
         <div className="row">
           <div className="col">Name: test</div>
           <div className="col">Phone number: xxx-xxx-xxxx</div>
-          <div className="col">Email: test@test.com</div>
-        </div>
-
-        <div className="row">
           <div className="col">twitter handle: fake-twitter-guy</div>
-          <div className="col">LinkdIN: www.linkdin.com</div>
-          <div className="col">Personal Website: mywebsite.com</div>
-          <div className="col">Github: Github.com</div>
+          <p>Email: test@test.com</p>
+          <p>LinkdIN: www.linkdin.com</p>
+          <p>Personal Website: mywebsite.com</p>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/e/ea/Test.gif"></img>
+          <p>Github: Github.com</p>
         </div>
       </div>
 
+      <div className="container">
+        <h2>Experience</h2>
+        <p>Start Date: yesterday</p>
+        <p>End Date: Today</p>
+        <p>Job Title: Winner</p>
+        <p>Company name: That one company</p>
+        <h3>Details: </h3>
+      </div>
       <div className="container">
         <h2>Experience</h2>
         <p>Start Date: yesterday</p>
@@ -41,6 +47,24 @@ export function Profile() {
         <p>Name: cappy the stone</p>
         <p>URL: mycapstone.com</p>
         <h3>Details: </h3>
+      </div>
+      <div className="container">
+        <h2>Capstone</h2>
+        <p>Name: cappy the stone</p>
+        <p>URL: mycapstone.com</p>
+        <h3>Details: </h3>
+      </div>
+
+      <div className="container">
+        <h2>Skills</h2>
+        <p>eating</p>
+        <p>swallowing</p>
+        <p>munching</p>
+        <p>self nourishing</p>
+        <p></p>
+        <p>ingesting</p>
+        <p>consuming</p>
+        <p>intaking sustinance</p>
       </div>
 
       <div className="container">
