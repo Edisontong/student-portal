802b57a1c1ff20bfcf8649318dc829208ad665df
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-03-15 11:53:50 -0600

    added login page

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,10 @@
+import { Login } from "./Login";
+
 export function Content() {
+  const logins = [{ id: 1, email: "test@test.com", password: "password" }];
   return (
     <div>
+      <Login logins={logins} />
       <h1>Welcome to our resume app!</h1>
     </div>
   );
