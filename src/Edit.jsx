a40ada040c1c435ba2b2992e18fe88bcede8ccda
Nicholas Gonzalez <thisnickg194@gmail.com>
2023-03-17 13:08:52 -0500

    corrected api reference variables

--- src/Edit.jsx
+++ src/Edit.jsx
@@ -18,9 +18,21 @@ export function Edit(props) {
           <hr />
           <h3>Student Info</h3>
           <label htmlFor="firstInput">First Name</label>
-          <input name="first" type="text" className="form-control" id="firstInput" defaultValue={props.student.first} />
+          <input
+            name="first_name"
+            type="text"
+            className="form-control"
+            id="firstInput"
+            defaultValue={props.student.first_name}
+          />
           <label htmlFor="lastInput">Last Name</label>
-          <input name="last" type="text" className="form-control" id="lastInput" defaultValue={props.student.last} />
+          <input
+            name="last_name"
+            type="text"
+            className="form-control"
+            id="lastInput"
+            defaultValue={props.student.last_name}
+          />
           <label htmlFor="exampleInputEmail1">Email address</label>
           <input
             name="email"
@@ -31,40 +43,52 @@ export function Edit(props) {
             defaultValue={props.student.email}
           />
           <label htmlFor="phoneInput">Phone Number</label>
-          <input name="phone" type="text" className="form-control" id="phoneInput" defaultValue={props.student.phone} />
+          <input
+            name="phone_number"
+            type="text"
+            className="form-control"
+            id="phoneInput"
+            defaultValue={props.student.phone_number}
+          />
           <label htmlFor="bioInput">Bio</label>
-          <input name="bio" type="text" className="form-control" id="bioInput" defaultValue={props.student.bio} />
+          <input
+            name="short_bio"
+            type="text"
+            className="form-control"
+            id="bioInput"
+            defaultValue={props.student.short_bio}
+          />
           <label htmlFor="linkedinInput">LinkedIN URL</label>
           <input
-            name="linkedin"
+            name="linkedin_url"
             type="text"
             className="form-control"
             id="linkedinInput"
-            defaultValue={props.student.linkedin}
+            defaultValue={props.student.linkedin_url}
           />
           <label htmlFor="twitterInput">Twitter Handle</label>
           <input
-            name="twitter"
+            name="twitter_handle"
             type="text"
             className="form-control"
             id="twitterInput"
-            defaultValue={props.student.twitter}
+            defaultValue={props.student.twitter_handle}
           />
           <label htmlFor="websiteInput">Personal Website URL</label>
           <input
-            name="website"
+            name="personal_blog_website_url"
             type="text"
             className="form-control"
             id="websiteInput"
-            defaultValue={props.student.website}
+            defaultValue={props.student.personal_blog_website_url}
           />
-          <label htmlFor="githubInput">Personal Website URL</label>
+          <label htmlFor="githubInput">Github URL</label>
           <input
-            name="github"
+            name="github_url"
             type="text"
             className="form-control"
             id="githubInput"
-            defaultValue={props.student.github}
+            defaultValue={props.student.github_url}
           />
           <label htmlFor="photoInput">Photo URL</label>
           <input name="photo" type="text" className="form-control" id="photoInput" defaultValue={props.student.photo} />
@@ -81,15 +105,45 @@ export function Edit(props) {
               <hr />
               <h3>Education {x + 1}</h3>
               <label htmlFor="startInput">Start Date</label>
-              <input type="text" className="form-control" id="startInput" defaultValue={education.start} />
+              <input
+                name="start_date"
+                type="text"
+                className="form-control"
+                id="startInput"
+                defaultValue={education.start_date}
+              />
               <label htmlFor="endInput">End Date</label>
-              <input type="text" className="form-control" id="endInput" defaultValue={education.start} />
+              <input
+                name="end_date"
+                type="text"
+                className="form-control"
+                id="endInput"
+                defaultValue={education.end_date}
+              />
               <label htmlFor="universityInput">School Name</label>
-              <input type="text" className="form-control" id="universityInput" defaultValue={education.university} />
+              <input
+                name="university_name"
+                type="text"
+                className="form-control"
+                id="universityInput"
+                defaultValue={education.university_name}
+              />
               <label htmlFor="degreeInput">Degree</label>
-              <input type="text" className="form-control" id="degreeInput" defaultValue={education.details} />
+              <input
+                name="degree"
+                type="text"
+                className="form-control"
+                id="degreeInput"
+                defaultValue={education.degree}
+              />
               <label htmlFor="detailsInput">Details</label>
-              <input type="text" className="form-control" id="detailsInput" defaultValue={education.details} />
+              <input
+                name="details"
+                type="text"
+                className="form-control"
+                id="detailsInput"
+                defaultValue={education.details}
+              />
             </div>
             <button type="submit" className="btn btn-primary">
               Submit
@@ -105,15 +159,45 @@ export function Edit(props) {
               <hr />
               <h3>Experience {x + 1}</h3>
               <label htmlFor="startInput">Start Date</label>
-              <input type="text" className="form-control" id="startInput" defaultValue={experience.start} />
+              <input
+                name="start_date"
+                type="text"
+                className="form-control"
+                id="startInput"
+                defaultValue={experience.start_date}
+              />
               <label htmlFor="endInput">End Date</label>
-              <input type="text" className="form-control" id="endInput" defaultValue={experience.start} />
+              <input
+                name="end_date"
+                type="text"
+                className="form-control"
+                id="endInput"
+                defaultValue={experience.end_date}
+              />
               <label htmlFor="companyInput">Company Name</label>
-              <input type="text" className="form-control" id="companyInput" defaultValue={experience.company} />
+              <input
+                name="company_name"
+                type="text"
+                className="form-control"
+                id="companyInput"
+                defaultValue={experience.company_name}
+              />
               <label htmlFor="titleInput">Job Title</label>
-              <input type="text" className="form-control" id="titleInput" defaultValue={experience.details} />
+              <input
+                name="job_title"
+                type="text"
+                className="form-control"
+                id="titleInput"
+                defaultValue={experience.job_title}
+              />
               <label htmlFor="detailsInput">Details</label>
-              <input type="text" className="form-control" id="detailsInput" defaultValue={experience.details} />
+              <input
+                name="details"
+                type="text"
+                className="form-control"
+                id="detailsInput"
+                defaultValue={experience.details}
+              />
             </div>
             <button type="submit" className="btn btn-primary">
               Submit
@@ -129,11 +213,17 @@ export function Edit(props) {
               <hr />
               <h3>Capstone {x + 1}</h3>
               <label htmlFor="nameInput">Name</label>
-              <input type="text" className="form-control" id="nameInput" defaultValue={capstone.name} />
+              <input name="name" type="text" className="form-control" id="nameInput" defaultValue={capstone.name} />
               <label htmlFor="descriptionInput">Description</label>
-              <input type="text" className="form-control" id="descriptionInput" defaultValue={capstone.description} />
+              <input
+                name="description"
+                type="text"
+                className="form-control"
+                id="descriptionInput"
+                defaultValue={capstone.description}
+              />
               <label htmlFor="urlInput">URL</label>
-              <input type="text" className="form-control" id="urlInput" defaultValue={capstone.url} />
+              <input name="url" type="text" className="form-control" id="urlInput" defaultValue={capstone.url} />
             </div>
             <button type="submit" className="btn btn-primary">
               Submit
